a8a712c4312957c46fbc2862b90658f13af30dcb
James <yuan.james@outlook.com>
2023-04-01 01:02:42 +0100

    .

--- james/split_file.py
+++ james/split_file.py
@@ -11,14 +11,17 @@ with open(path + "webtext.test.model.sorted.jsonl") as f:
     df = pd.read_json(f, lines=True)
 
 var_len = 0
+gap = 200
 for index, row in tqdm(df.iterrows()):
-    if row['token_len'] >= var_len and row["token_len"] < var_len + 200:
+    if row['token_len'] >= var_len and row["token_len"] < var_len + gap:
         with jsonlines.open(
                 gen_path + "webtext.test.split." + str(var_len) + ".jsonl",
                 "a") as w:
             w.write({'text': row["text"]})
     else:
         var_len += 200
+        if var_len + gap == 1000:
+            gap = 225
         with jsonlines.open(
                 gen_path + "webtext.test.split." + str(var_len) + ".jsonl",
                 "a") as w:
