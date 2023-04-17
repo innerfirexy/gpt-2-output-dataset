7c0b192381b8ed1a1df52c64494daf1e927101ed
xy <innerfirexy@gmail.com>
2023-04-17 14:56:12 -0700

    model size bar plot

--- xy/barplot.R
+++ xy/barplot.R
@@ -1,5 +1,7 @@
 require("ggplot2")
 require("data.table")
+require("stringr")
+require("patchwork")
 
 # Read Bai's data on GPT-2
 pso <- c(0.374,0.395,0.392,0.397,0.381,0.398)
@@ -70,3 +72,73 @@ p3 <- ggplot(dt_k40, aes(x = model, y = value, fill = variable)) +
                                "diversity_norm" = "#0072B2", "bleu_norm" = "#D55E00",
                                "selfbleu_norm" = "#CC79A7", "mauve_norm" = "#000000"))
 ggsave("gpt2_barplot_k40.pdf", plot=p3)
+
+
+####
+# Results from QUANTATIVE_RESULTS table
+####
+PSO_bloom_sm <- c(0.7021830655, 0.72752)
+PSO_bloom_bg <- c(0.7085967177, 0.728061614)
+CORR_bloom_sm <- c(0.6144489458, 0.6972528097)
+CORR_bloom_bg <- c(0.6288706257, 0.7150556415)
+SAM_bloom_sm <- c(0.2811285446, 0.251020925)
+SAM_bloom_bg <- c(0.2775527271, 0.2443721022)
+SPEAR_bloom_sm <- c(0.04771712522, 0.03185782206)
+SPEAR_bloom_bg <- c(0.03822721501, 0.01861278898)
+
+PSO_opt_sm <- c(0.707349171, 0.728212541)
+PSO_opt_bg <- c(0.708433333, 0.730395623)
+CORR_opt_sm <- c(0.66619401, 0.719752813)
+CORR_opt_bg <- c(0.662843547, 0.712448585)
+SAM_opt_sm <- c(0.263160641, 0.242144144)
+SAM_opt_bg <- c(0.264506319, 0.24539481)
+SPEAR_opt_sm <- c(0.03843578, 0.015199294)
+SPEAR_opt_bg <- c(0.04524173, 0.017465422)
+
+length_group <- c("0-200", "201-400")
+length_weight <- c(0.5, 0.5)
+
+df <- data.frame(PSO_bloom_sm, PSO_bloom_bg, CORR_bloom_sm, CORR_bloom_bg,
+                 SAM_bloom_sm, SAM_bloom_bg, SPEAR_bloom_sm, SPEAR_bloom_bg,
+                    PSO_opt_sm, PSO_opt_bg, CORR_opt_sm, CORR_opt_bg,
+                    SAM_opt_sm, SAM_opt_bg, SPEAR_opt_sm, SPEAR_opt_bg,
+                 length_group, length_weight)
+dt <- data.table(df)
+dt <- melt(dt, id.vars = c("length_group", "length_weight"), variable.name = "metric")
+
+dt.avg <- dt[, .(avg = weighted.mean(value, length_weight)), by = metric]
+dt.avg$domain <- "news"
+
+dt.avg$size <- sapply(str_detect(dt.avg$metric, "_sm"),
+                       FUN = function (x) {if(x) "small" else "big"})
+dt.avg$model <- "BLOOM"
+dt.avg[str_detect(metric, "opt"), model := "OPT"]
+
+dt.avg$metric_name <- sapply(str_split(dt.avg$metric, "_"), FUN = function(x) x[1])
+
+# Bar plot
+p1 <- ggplot(dt.avg[metric_name=="PSO"], aes(x = model, y = avg, fill = size)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  coord_cartesian(ylim = c(0.7, 0.72)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric_name)
+
+p2 <- ggplot(dt.avg[metric_name=="CORR"], aes(x = model, y = avg, fill = size)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  coord_cartesian(ylim = c(0.6, 0.75)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric_name)
+
+p3 <- ggplot(dt.avg[metric_name=="SAM"], aes(x = model, y = avg, fill = size)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  coord_cartesian(ylim = c(0.2, 0.3)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric_name)
+
+p4 <- ggplot(dt.avg[metric_name=="SPEAR"], aes(x = model, y = avg, fill = size)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  coord_cartesian(ylim = c(0, 0.05)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric_name)
+
+p <- p1+ p2+ p3+ p4 + guide_area() + plot_layout(ncol=5, guides = "collect")
