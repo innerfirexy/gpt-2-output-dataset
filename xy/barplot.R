be0614817e4ba956e66132fcd0281832d90487f4
Yang Xu <innerfirexy@gmail.com>
2023-04-25 09:35:47 -0700

    plot

--- xy/barplot.R
+++ xy/barplot.R
@@ -157,81 +157,56 @@ dt.melt.avg[metric=="PSO" & model=="bloom_sm" & domain=="news",]
 # 1:   news    PSO bloom_sm 0.7022845
 # Now got correct number
 
+# Add split `model` to `model name` and `model size`
+dt.melt.avg[, `:=`(modelName = gsub("_.*", "", model),
+                   modelSize = gsub(".*_", "", model))]
+dt.melt.avg[, modelName := factor(modelName, levels = c("gpt2", "opt", "bloom"))]
+dt.melt.avg[, modelSize := factor(modelSize, levels = c("sm", "bg"))]
+dt.melt.avg[, metric := factor(metric, levels = c("PSO", "CORR", "SAM", "SPEAR"))]
 
-
-
-# News
-PSO_bloom_sm <- c(0.7021830655, 0.72752)
-PSO_bloom_bg <- c(0.7085967177, 0.728061614)
-CORR_bloom_sm <- c(0.6144489458, 0.6972528097)
-CORR_bloom_bg <- c(0.6288706257, 0.7150556415)
-SAM_bloom_sm <- c(0.2811285446, 0.251020925)
-SAM_bloom_bg <- c(0.2775527271, 0.2443721022)
-SPEAR_bloom_sm <- c(0.04771712522, 0.03185782206)
-SPEAR_bloom_bg <- c(0.03822721501, 0.01861278898)
-
-PSO_opt_sm <- c(0.707349171, 0.728212541)
-PSO_opt_bg <- c(0.708433333, 0.730395623)
-CORR_opt_sm <- c(0.66619401, 0.719752813)
-CORR_opt_bg <- c(0.662843547, 0.712448585)
-SAM_opt_sm <- c(0.263160641, 0.242144144)
-SAM_opt_bg <- c(0.264506319, 0.24539481)
-SPEAR_opt_sm <- c(0.03843578, 0.015199294)
-SPEAR_opt_bg <- c(0.04524173, 0.017465422)
-
-PSO_gpt2_sm <- c(0.7093288, 0.737018249)
-PSO_gpt2_bg <- c(0.709373565, 0.733150674)
-CORR_gpt2_sm <- c(0.647413501, 0.725049197)
-CORR_gpt2_bg <- c(0.626997834, 0.69114533)
-SAM_gpt2_sm <- c(0.270093568, 0.239829325)
-SAM_gpt2_bg <- c(0.279523374, 0.255399434)
-SPEAR_gpt2_sm <- c(0.046212277, 0.025752112)
-SPEAR_gpt2_bg <- c(0.045604807, 0.021786918)
-
-length_group <- c("0-200", "201-400")
-length_weight <- c(0.5, 0.5)
-
-df <- data.frame(PSO_bloom_sm, PSO_bloom_bg, CORR_bloom_sm, CORR_bloom_bg,
-                 SAM_bloom_sm, SAM_bloom_bg, SPEAR_bloom_sm, SPEAR_bloom_bg,
-                    PSO_opt_sm, PSO_opt_bg, CORR_opt_sm, CORR_opt_bg,
-                    SAM_opt_sm, SAM_opt_bg, SPEAR_opt_sm, SPEAR_opt_bg,
-                 length_group, length_weight)
-dt <- data.table(df)
-dt <- melt(dt, id.vars = c("length_group", "length_weight"), variable.name = "metric")
-
-dt.avg <- dt[, .(avg = weighted.mean(value, length_weight)), by = metric]
-dt.avg$domain <- "news"
-
-dt.avg$size <- sapply(str_detect(dt.avg$metric, "_sm"),
-                       FUN = function (x) {if(x) "small" else "big"})
-dt.avg$model <- "BLOOM"
-dt.avg[str_detect(metric, "opt"), model := "OPT"]
-
-dt.avg$metric_name <- sapply(str_split(dt.avg$metric, "_"), FUN = function(x) x[1])
-
-# Bar plot
-p1 <- ggplot(dt.avg[metric_name=="PSO"], aes(x = model, y = avg, fill = size)) +
+# score ~ modelSize bar plot
+p1 <- ggplot(dt.melt.avg[metric=="PSO" & domain=="news"], aes(x = modelName, y = score, fill = modelSize)) +
   geom_bar(stat = "identity", position = "dodge") +
-  coord_cartesian(ylim = c(0.7, 0.72)) +
+  coord_cartesian(ylim = c(0.65, 0.75)) +
+  scale_fill_manual(values = c("sm" = "#00BFC4", "bg" = "#F8766D")) + # green:"#7CAE00" blue:"#00BFC4" red:"#F8766D" purple:"C77CFF"
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric_name)
+  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric)
+# ggsave("PSO_news_modelSize.pdf", plot=p1)
 
-p2 <- ggplot(dt.avg[metric_name=="CORR"], aes(x = model, y = avg, fill = size)) +
+p2 <- ggplot(dt.melt.avg[metric=="CORR" & domain=="news"], aes(x = modelName, y = score, fill = modelSize)) +
   geom_bar(stat = "identity", position = "dodge") +
-  coord_cartesian(ylim = c(0.6, 0.75)) +
+  # coord_cartesian(ylim = c(0.6, 0.75)) +
+  scale_fill_manual(values = c("sm" = "#00BFC4", "bg" = "#F8766D")) + # green:"#7CAE00" blue:"#00BFC4" red:"#F8766D" purple:"C77CFF"
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric_name)
+  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric)
+# ggsave("CORR_news_modelSize.pdf", plot=p2)
 
-p3 <- ggplot(dt.avg[metric_name=="SAM"], aes(x = model, y = avg, fill = size)) +
+p3 <- ggplot(dt.melt.avg[metric=="SAM" & domain=="news"], aes(x = modelName, y = score, fill = modelSize)) +
   geom_bar(stat = "identity", position = "dodge") +
-  coord_cartesian(ylim = c(0.2, 0.3)) +
+  # coord_cartesian(ylim = c(0.6, 0.75)) +
+  scale_fill_manual(values = c("sm" = "#00BFC4", "bg" = "#F8766D")) + # green:"#7CAE00" blue:"#00BFC4" red:"#F8766D" purple:"C77CFF"
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric_name)
+  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric)
+# ggsave("SAM_news_modelSize.pdf", plot=p3)
 
-p4 <- ggplot(dt.avg[metric_name=="SPEAR"], aes(x = model, y = avg, fill = size)) +
+p4 <- ggplot(dt.melt.avg[metric=="SPEAR" & domain=="news"], aes(x = modelName, y = score, fill = modelSize)) +
   geom_bar(stat = "identity", position = "dodge") +
-  coord_cartesian(ylim = c(0, 0.05)) +
+  # coord_cartesian(ylim = c(0.6, 0.75)) +
+  scale_fill_manual(values = c("sm" = "#00BFC4", "bg" = "#F8766D")) + # green:"#7CAE00" blue:"#00BFC4" red:"#F8766D" purple:"C77CFF"
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric_name)
+  labs(x = "Model", y = "Score", fill = "Size") + facet_wrap(~metric)
+# ggsave("SPEAR_news_modelSize.pdf", plot=p4)
 
-p <- p1+ p2+ p3+ p4 + guide_area() + plot_layout(ncol=5, guides = "collect")
+p <- p1 + p2 + p3 + p4 + guide_area() + plot_layout(ncol=5, guides = "collect")
+ggsave("news_modelSize.pdf", plot=p, width=20, height=5)
+
+# Using facet_grid
+target_metrics <- c("PSO", "CORR", "SAM", "SPEAR")
+
+
+p <- ggplot(dt.melt.avg[metric %in% target_metrics], aes(x = modelName, y = score, fill = modelSize)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(values = c("sm" = "#00BFC4", "bg" = "#F8766D")) + # green:"#7CAE00" blue:"#00BFC4" red:"#F8766D" purple:"C77CFF"
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Model", y = "Score", fill = "Size") + facet_grid(domain~metric, scales="free_y")
+ggsave("modelSize_facet_grid.pdf", plot=p)
