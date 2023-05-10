1b9ee431cc677d610dd80cafd15d4d012cb98513
Yang Xu <innerfirexy@gmail.com>
2023-05-10 00:23:35 -0700

    add typical plots

--- xy/interpret.R
+++ xy/interpret.R
@@ -2,6 +2,53 @@ require("ggplot2")
 require("data.table")
 require("mgcv")
 require("splus2R")
+require("patchwork")
+
+####
+# Find typical spectrum from all 3 models: gpt2-xl, opt, bloom, and gold standard
+d.gpt2.fft <- fread("../data/data_gpt2_old/webtext.test.model=gpt2-xl.fft.csv")
+d.opt.fft <- fread("../data/experiments_data/opt/webtext.train_opt_6.7b_top_50_news.sorted.split.400.fft.csv")
+d.bloom.fft <- fread("../data/experiments_data/bloom/split_news/webtext.train.model=.bloom_7b1.news.sorted.split.400.fft.csv")
+d.gs.fft <- fread("../data/gs_james/gs_wiki/webtext.train.model=.wiki_3.fft.csv")
+
+# Four colors
+# gpt2-xl: #1f77b4
+# opt: #ff7f0e
+# bloom: #2ca02c
+# gs: #d62728
+colors <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728")
+
+p.gpt2 <- ggplot(d.gpt2.fft, aes(freq, power)) +
+  geom_smooth(color=colors[1]) +
+  theme_bw() + theme(plot.title = element_text(hjust = 0.5, vjust=-8, size = 20)) +
+  ggtitle("GPT2-XL") +
+  labs(x = bquote(omega[k]), y = bquote(X(omega[k])))
+ggsave("typical_spectrum_gpt2-xl.pdf", plot=p.gpt2, width=4, height=4)
+
+p.opt <- ggplot(d.opt.fft, aes(freq, power)) +
+  geom_smooth(color=colors[2]) +
+  theme_bw() + theme(plot.title = element_text(hjust = 0.5, vjust=-8, size = 20)) +
+  ggtitle("OPT") +
+  labs(x = bquote(omega[k]), y = bquote(X(omega[k])))
+ggsave("typical_spectrum_opt.pdf", plot=p.opt, width=4, height=4)
+
+p.bloom <- ggplot(d.bloom.fft, aes(freq, power)) +
+  geom_smooth(color=colors[3]) +
+  theme_bw() + theme(plot.title = element_text(hjust = 0.5, vjust=-8, size = 20)) +
+  ggtitle("BLOOM") +
+  labs(x = bquote(omega[k]), y = bquote(X(omega[k])))
+ggsave("typical_spectrum_bloom.pdf", plot=p.bloom, width=4, height=4)
+
+p.gs <- ggplot(d.gs.fft, aes(freq, power)) +
+  geom_smooth(color=colors[4]) +
+  theme_bw() + theme(plot.title = element_text(hjust = 0.5, vjust=-8, size = 20)) +
+  ggtitle("Human") +
+  labs(x = bquote(omega[k]), y = bquote(X(omega[k])))
+ggsave("typical_spectrum_gs.pdf", plot=p.gs, width=4, height=4)
+
+p <- p.gpt2 + p.opt + p.bloom + p.gs + plot_layout(ncol=2)
+ggsave("typical_spectra.pdf", plot=p, width=8, height=8)
+
 
 # Read the FFT data
 d.wbt.fft <- fread("../data/data_gpt2_old/webtext.test.model=gpt2.fft.csv")
