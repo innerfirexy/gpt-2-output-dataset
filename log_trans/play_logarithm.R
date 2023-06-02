e1e93bfbba2041e6354f92af603d36bb9789dc1b
xy <innerfirexy@gmail.com>
2023-06-02 12:36:52 +0800

    play

--- log_trans/play_logarithm.R
+++ log_trans/play_logarithm.R
@@ -2,14 +2,17 @@ require("data.table")
 require("ggplot2")
 
 x <- seq(0.001, 0.999, 0.001)
-logx <- -log(x)
-loglogx <- log(logx)
+neg_log_x <- -log(x)
+log_neg_log_x <- log(neg_log_x)
 
 # Plot
-d <- data.table(x = x, logx = logx, loglogx = loglogx)
+d <- data.table(x = x, 
+                neg_log_x = neg_log_x, 
+                log_neg_log_x = log_neg_log_x)
 d.melt <- melt(d, id.vars = "x", variable.name = "y_type", value.name = "y")
-d.melt$y_type <- factor(d.melt$y_type, levels = c("x", "logx", "loglogx"))
 
 p <- ggplot(d.melt, aes(x,y)) + 
-    geom_line(aes(color=y_type, linetype=y_type)) + theme_bw()
+    geom_line(aes(color=y_type, linetype=y_type)) + theme_bw() +
+    scale_color_brewer(palette="Set1", labels=c("-log(x)", "log(-log(x))")) + 
+    scale_linetype_discrete(labels=c("-log(x)", "log(-log(x))"))
 ggsave("logx_loglogx.pdf", plot=p)
